f7a5bdd8158a798391aa6c7a0f27fb46507bed53
unknown <aldin_mikara@hotmail.com>
2021-07-24 01:36:31 +0200

    allowed cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+var cors = require('cors');
 const quiz = require('./quiz')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(
   express.json({
