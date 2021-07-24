ae57167cbc5dc97280ed9e656caef801b022d0a9
unknown <aldin_mikara@hotmail.com>
2021-07-25 00:41:56 +0200

    added animation for getting the answer wrong

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -2,7 +2,7 @@
   <div class="flex card flex-row justify-center items-center py-5">
     <PlayButton :id="id" />
     <form class="self-end" @submit.prevent="handleSubmit">
-      <input class="h-6 relative right-5 rounded self-end answer-input" v-model="input" :disabled="isDisabled ? true : false" placeholder="type name of the game.."/>
+      <input ref="inputField" class="h-6 relative right-5 rounded self-end answer-input" v-model="input" :disabled="isDisabled ? true : false" placeholder="type name of the game.."/>
     </form>
   </div>
 </template>
@@ -31,6 +31,27 @@ export default {
           .then((res) => {
             this.isCorrect = res.data.success;
             if (!this.isCorrect) {
+              const { inputField } = this.$refs;
+              inputField.animate([
+                {
+                  transform: 'translate(1px, 1px) rotate(0deg)',
+                  color: 'red',
+                },
+                { transform: 'translate(-1px, -2px) rotate(-1deg)' },
+                { transform: 'translate(-3px, 0px) rotate(1deg)' },
+                { transform: 'translate(3px, 2px) rotate(0deg)' },
+                { transform: 'translate(1px, -1px) rotate(1deg)' },
+                { transform: 'translate(-1px, 2px) rotate(-1deg)' },
+                { transform: 'translate(-3px, 1px) rotate(0deg)' },
+                { transform: 'translate(3px, 1px) rotate(-1deg)' },
+                { transform: 'translate(-1px, -1px) rotate(1deg)' },
+                { transform: 'translate(1px, 2px) rotate(0deg)' },
+                { transform: 'translate(1px, -2px) rotate(-1deg)' },
+              ],
+              {
+                easing: 'ease-out',
+                duration: 500,
+              });
               this.$store.commit('removeLife');
             } else {
               this.$store.commit('incrementScore');
